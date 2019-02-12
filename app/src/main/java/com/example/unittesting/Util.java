92746dc5af4f9120937d52efd99995a9678f63e2
Nafisisty <nafisisty@gmail.com>
2019-02-12 16:48:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/unittesting/Util.java
@@ -0,0 +1,20 @@
+package com.example.unittesting;
+
+public class Util {
+
+    public static String someMethod() {
+        return "";
+    }
+
+    public String getName() {
+        return "Bob";
+    }
+
+    public String getModName(String s1, boolean toMod) {
+        if(toMod) {
+            return s1+ " moded";
+        }else {
+            return s1;
+        }
+    }
+}
---
+++ app/src/test/java/com/example/unittesting/ExampleUnitTest.java
@@ -0,0 +1,20 @@
+package com.example.unittesting;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+
+    Util util = new Util();
+    @Test
+    public void testGetModName() {
+        assertEquals("Bob",util.getModName("Bob", false));
+        assertEquals("Bob moded",util.getModName("Bob", true));
+    }
+}
